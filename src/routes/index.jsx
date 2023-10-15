6bd7c8331b5f3a235c8cfaf136d59d633ec9a05b
saputhebeast <sapumalwijekoon4@gmail.com>
2023-10-15 20:11:10 +0530

    feat: resolved conflicts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Login, Example } from "../pages";
-<<<<<<< HEAD
 import { AdminExample, SupplierExample } from "../dashboards";
 import { StaffPage } from "@/dashboards/staff";
-=======
-import { StaffExample, AdminExample, SupplierExample } from "../dashboards";
->>>>>>> 00c1858bf99a0288206632d2ed4b2d97b92365b1
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -16,11 +12,7 @@ const AnimatedRoutes = () => {
       <Route path="/example" element={<Example />} />
       <Route path="/supplier" element={<SupplierExample />} />
       <Route path="/admin" element={<AdminExample />} />
-<<<<<<< HEAD
       <Route path="/staff" element={<StaffPage />} />
-=======
-      <Route path="/staff" element={<StaffExample />} />
->>>>>>> 00c1858bf99a0288206632d2ed4b2d97b92365b1
     </Routes>
   );
 };
