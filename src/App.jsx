3c8283732dc1865b38575d61a9f25d354b28e235
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2023-10-15 11:55:58 +0530

    Feat/ Added actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
 import { default as AnimatedRoutes } from "@/routes/index.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    <Router>
       <AnimatedRoutes />
-      </Router>
     </>
   )
 }
