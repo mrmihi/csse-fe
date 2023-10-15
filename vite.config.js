f316e3252e242f4d5423f27c8a1d9882e6bc80b7
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2023-10-15 12:04:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "csse-fe",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
