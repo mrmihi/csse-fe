60a9e92ec1b6da5b11cf8448a489f59714dcefee
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2023-10-16 10:58:35 +0530

    Fix(): Fixed staff ui overlapping and routes

--- src/dashboards/staff/staff-page.jsx
+++ src/dashboards/staff/staff-page.jsx
@@ -3,8 +3,14 @@ import StaffSidebar from "./components/sidebar";
 const StaffPage = () => {
   return (
     <>
-      <StaffSidebar />
-      <h1>Hello World from staff page</h1>
+      <div className="grid lg:grid-cols-6">
+        <div>
+          <StaffSidebar />
+        </div>
+        <div className="col-span-3 lg:col-span-5 lg:border-l mt-10 mr-10">
+          <h1>Hello World from staff page</h1>
+        </div>
+      </div>
     </>
   );
 };
--- src/dashboards/staff/staff-requisitions.jsx
+++ src/dashboards/staff/staff-requisitions.jsx
@@ -15,25 +15,31 @@ const StaffRequisition = () => {
 
   return (
     <>
-      <StaffSidebar />
-      <div className="flex justify-center">
-        <Tabs defaultValue="account" className="w-[1080px]">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="pending" onClick={showPendingTable}>
-              Pending
-            </TabsTrigger>
-            <TabsTrigger value="approved" onClick={showApprovedTable}>
-              Approved
-            </TabsTrigger>
-            <TabsTrigger value="declined" onClick={showDeclinedTable}>
-              Declined
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
+      <div className="grid lg:grid-cols-6">
+        <div>
+          <StaffSidebar />
+        </div>
+        <div className="col-span-3 lg:col-span-5 lg:border-l mt-10 mr-10">
+          <div>
+            <Tabs defaultValue="account" className="w-[1080px]">
+              <TabsList className="grid w-full grid-cols-3">
+                <TabsTrigger value="pending" onClick={showPendingTable}>
+                  Pending
+                </TabsTrigger>
+                <TabsTrigger value="approved" onClick={showApprovedTable}>
+                  Approved
+                </TabsTrigger>
+                <TabsTrigger value="declined" onClick={showDeclinedTable}>
+                  Declined
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+          {activeTable === "pending" && <RequisitionPendingOrder />}
+          {activeTable === "approved" && <RequisitionApprovedOrder />}
+          {activeTable === "declined" && <RequisitionDeclinedOrder />}
+        </div>
       </div>
-      {activeTable === "pending" && <RequisitionPendingOrder />}
-      {activeTable === "approved" && <RequisitionApprovedOrder />}
-      {activeTable === "declined" && <RequisitionDeclinedOrder />}
     </>
   );
 };
